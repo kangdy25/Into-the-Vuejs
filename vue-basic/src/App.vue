6cae4dbc8b8208b3e21d4aa34268ff6d56624ef7
Kangdy <canfrog@naver.com>
2026-04-22 17:13:31 +0900

    [Vue Basic] Interpolation

--- vue-basic/src/App.vue
+++ vue-basic/src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import Interpolation from "./components/Interpolation.vue";
 </script>
 
 <template>
-  <HelloWorld />
+    <Interpolation />
 </template>
---
+++ vue-basic/src/components/Interpolation.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h1>배워볼 프로그래밍 프레임워크: {{ language }}</h1>
+        <h1>03장 Vue.js 기초 문법 이해하기</h1>
+        <h2>컨텐츠: {{ content }}</h2>
+        <h3>작성자: {{ author }}</h3>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            language: "Vue.js",
+            content: "데이터 정의와 데이터 보간법",
+            author: "홍길동",
+        };
+    },
+};
+</script>
