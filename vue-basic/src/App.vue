ac3891bbb7d6bc2851521d7e3970944a841243c3
Kangdy <canfrog@naver.com>
2026-04-23 11:40:10 +0900

    [Vue Basic] Event Handling

--- vue-basic/src/App.vue
+++ vue-basic/src/App.vue
@@ -3,11 +3,13 @@ import DeclarativeRendering from "./components/DeclarativeRendering.vue";
 import Interpolation from "./components/Interpolation.vue";
 import ConditionalRendering from "./components/ConditionalRendering.vue";
 import ListRendering from "./components/ListRendering.vue";
+import EventHandling from "./components/EventHandling.vue";
 </script>
 
 <template>
     <!-- <Interpolation /> -->
     <!-- <DeclarativeRendering /> -->
     <!-- <ConditionalRendering /> -->
-    <ListRendering/>
+    <!-- <ListRendering/> -->
+    <EventHandling />
 </template>
---
+++ vue-basic/src/components/EventHandling.vue
@@ -0,0 +1,17 @@
+<template>
+    <!-- v-on 디렉티브를 사용하여 클릭 이벤트 핸들러를 바인딩 -->
+    <button @click="handleClick">클릭하세요</button>
+</template>
+
+<script>
+export default {
+    methods: {
+        handleClick(event) {
+            console.log(event); // 이벤트 객체 전체 출력
+            console.log("클릭한 좌표:", event.clientX, event.clientY);
+        },
+    },
+};
+</script>
+
+<style scoped></style>
