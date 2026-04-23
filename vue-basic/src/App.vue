f24671b4915f0e7517957804a7061d0e178b1044
Kangdy <canfrog@naver.com>
2026-04-23 11:23:38 +0900

    [Vue Basic] Conditional Rendering

--- vue-basic/src/App.vue
+++ vue-basic/src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import DeclarativeRendering from "./components/DeclarativeRendering.vue";
 import Interpolation from "./components/Interpolation.vue";
+import ConditionalRendering from "./components/ConditionalRendering.vue";
 </script>
 
 <template>
     <!-- <Interpolation /> -->
-    <DeclarativeRendering />
+    <!-- <DeclarativeRendering /> -->
+    <ConditionalRendering />
 </template>
---
+++ vue-basic/src/components/ConditionalRendering.vue
@@ -0,0 +1,36 @@
+<template>
+    <h1>isVisible 값: {{ isVisible }}</h1>
+    <h1 v-if="isVisible">이 문장은 isVisible 값이 {{ isVisible }}일 경우에만 보입니다.</h1>
+
+    <button @click="toggleVisibility">버튼</button>
+
+    <hr/>
+
+    <h1 v-if="score > 90">Excellent!!</h1>
+    <h1 v-else-if="score > 75">Good!</h1>
+    <h1 v-else>Needs Improvement.</h1>
+
+    <hr/>
+    <h1 v-if="isVisible">v-if: {{ isVisible }}</h1>
+    <h1 v-show="isVisible">v-show: {{ isVisible }}</h1>
+    <h1 v-show="!isVisible">v-show: {{ isVisible }}</h1>
+    <button @click="toggleVisibility">버튼</button>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            isVisible: true,
+            score: 70,
+        };
+    },
+    methods: {
+        toggleVisibility() {
+            this.isVisible = !this.isVisible;
+        },
+    },
+};
+</script>
+
+<style scoped></style>
