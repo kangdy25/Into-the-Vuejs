a1c06bc349b2c72c856b535afdbef4a36459b1a9
Kangdy <canfrog@naver.com>
2026-04-24 23:53:01 +0900

    [Vue Basic] LifeCycle - Creation

--- vue-basic/src/App.vue
+++ vue-basic/src/App.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
-import DeclarativeRendering from "./components/DeclarativeRendering.vue";
-import Interpolation from "./components/Interpolation.vue";
-import ConditionalRendering from "./components/ConditionalRendering.vue";
-import ListRendering from "./components/ListRendering.vue";
-import EventHandling from "./components/EventHandling.vue";
-import TwoWayDataBinding from "./components/TwoWayDataBinding.vue";
-import ComputedAttribute from "./components/ComputedAttribute.vue";
-import Watcher from "./components/Watcher.vue";
+import Creation from "./components/LifeCycle/Creation.vue";
+
+// import DeclarativeRendering from "./components/DeclarativeRendering.vue";
+// import Interpolation from "./components/Interpolation.vue";
+// import ConditionalRendering from "./components/ConditionalRendering.vue";
+// import ListRendering from "./components/ListRendering.vue";
+// import EventHandling from "./components/EventHandling.vue";
+// import TwoWayDataBinding from "./components/TwoWayDataBinding.vue";
+// import ComputedAttribute from "./components/ComputedAttribute.vue";
+// import Watcher from "./components/Watcher.vue";
 </script>
 
 <template>
@@ -17,5 +19,7 @@ import Watcher from "./components/Watcher.vue";
     <!-- <EventHandling /> -->
     <!-- <TwoWayDataBinding/> -->
     <!-- <ComputedAttribute/> -->
-    <Watcher />
+    <!-- <Watcher /> -->
+
+    <Creation />
 </template>
---
+++ vue-basic/src/components/LifeCycle/Creation.vue
@@ -0,0 +1,29 @@
+<template>
+    <h1>{{ count }}</h1>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            count: 10,
+        };
+    },
+    /** 라이프사이클 훅(Hooks) */
+    /** 생성 단계(Creation) */
+    beforeCreate() {
+        console.log(
+            "라이프사이클 훅 - beforeCreate: 인스턴스가 생성된 직후, 데이터 초기화 이전",
+            this.count,
+        );
+    },
+    created() {
+        console.log(
+            "라이프사이클 훅 - created: 데이터와 이벤트가 초기화된 후",
+            this.count,
+        );
+    },
+};
+</script>
+
+<style scoped></style>
