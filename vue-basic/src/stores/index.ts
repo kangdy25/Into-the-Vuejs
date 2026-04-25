78f753d205610143f3730d4a89814802c8300964
Kangdy <canfrog@naver.com>
2026-04-25 19:27:13 +0900

    [Vue Basic] Pinia - Create Store

---
+++ vue-basic/src/stores/index.ts
@@ -0,0 +1,25 @@
+import { defineStore } from "pinia";
+
+export const useStore = defineStore("main", {
+    state: () => {
+        return {
+            title: "제목",
+            description: "설명",
+            count: 10,
+        };
+    },
+    getters: {
+        doubleCount: (state) => state.count * 2,
+        doublePlusOne(): number {
+            return this.doubleCount + 1;
+        },
+    },
+    actions: {
+        increment() {
+            this.count++;
+        },
+        randomSizeCounter() {
+            this.count = Math.round(100 * Math.random());
+        },
+    },
+});
