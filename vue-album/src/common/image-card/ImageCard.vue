e8b1347cc010320bf90ee7d33cf91ac643bdd21b
Kangdy <canfrog@naver.com>
2026-04-30 20:27:47 +0900

    [Vue Project - Album] Export Common Component

---
+++ vue-album/src/common/image-card/ImageCard.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="flex flex-col space-y-3">
+    <Skeleton class="h-[150px] w-[250px] rounded-xl" />
+    <div class="space-y-2">
+      <Skeleton class="h-4 w-[250px]" />
+      <Skeleton class="h-4 w-[200px]" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Skeleton } from "@/components/ui/skeleton";
+
+export default {
+  components: {
+    Skeleton,
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ vue-album/src/common/index.ts
@@ -0,0 +1,4 @@
+export { default as CommonHeader } from "./header/Header.vue";
+export { default as CommonNavigation } from "./navigation/Navigation.vue";
+export { default as CommonPagination } from "./pagination/Pagination.vue";
+export { default as CommonImageCard } from "./image-card/ImageCard.vue";
