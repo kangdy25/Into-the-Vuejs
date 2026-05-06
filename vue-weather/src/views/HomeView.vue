e0178954c8eb7a23d0baf56eb20fee0bac125730
Shineast <canfrog@naver.com>
2026-05-06 16:55:39 +0900

    [Vue Project - Weather] Set up a design system

--- vue-weather/src/views/HomeView.vue
+++ vue-weather/src/views/HomeView.vue
@@ -1,5 +1,18 @@
 <template>
-  <div>HomeView.vue 페이지 컴포넌트</div>
+  <div class="page">
+    HomeView.vue 페이지 컴포넌트
+    <div class="page_container">
+      <div></div>
+      <div
+        class="w-full h-full flex flex-col items-center justify-start pb-6 px-6 gap-6"
+      >
+        <!-- 상단 위젯 영역 -->
+        <div class="w-full flex items-center justify-start gap-6"></div>
+        <!-- 하단 위젯 영역 -->
+        <div class="w-full flex items-center gap-6"></div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts"></script>
