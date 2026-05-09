62cbf0cb3cdc22d832701e9539b6a4f90a63b59a
Kangdy <canfrog@naver.com>
2026-05-09 23:46:56 +0900

    [Vue Project - Weather] Bind Weather API data to the one week weather widget

--- vue-weather/src/components/widgets/OneWeekWeather.vue
+++ vue-weather/src/components/widgets/OneWeekWeather.vue
@@ -6,192 +6,47 @@
     </CardHeader>
     <CardContent class="flex flex-col w-full gap-1">
       <div
+        v-for="item in data"
+        :key="item.date"
         class="w-full flex items-center gap-7 bg-neutral-50 py-0 px-3 rounded-md"
       >
         <div class="w-fit h-10 flex items-center gap-2">
-          <img src="/assets/icons/1000d.svg" alt="" class="h-7 w-7" />
+          <img
+            v-if="item.isDay"
+            :src="`/assets/icons/${item.iconCode}d.svg`"
+            alt=""
+            class="h-7 w-7"
+          />
+          <img
+            v-else
+            :src="`/assets/icons/${item.iconCode}n.svg`"
+            alt=""
+            class="h-7 w-7"
+          />
           <div class="flex items-center gap-1 w-20">
             <div class="w-full h-full flex items-start gap-[2px]">
               <span
                 class="poppins-medium scroll-m-20 text-lg font-semibold tracking-tight text-red-600"
-                >25</span
+                >{{ item.maxTemp }}</span
               >
               <span class="text-xs font-normal mt-[3px]">&#8451;</span>
             </div>
             <div class="w-full h-full flex items-start gap-[2px]">
               <span
                 class="poppins-medium scroll-m-20 text-lg font-semibold tracking-tight text-sky-600"
-                >10</span
+                >{{ item.minTemp }}</span
               >
               <span class="text-xs font-normal mt-[3px]">&#8451;</span>
             </div>
           </div>
         </div>
         <div class="flex-1 flex items-center justify-end gap-5 mb-1">
-          <small class="text-sm leading-none">Nov 30</small>
-          <small class="text-sm leading-none">토요일</small>
-        </div>
-      </div>
-      <div
-        class="w-full flex items-center gap-7 bg-neutral-50 py-0 px-3 rounded-md"
-      >
-        <div class="w-fit h-10 flex items-center gap-2">
-          <img src="/assets/icons/1000d.svg" alt="" class="h-7 w-7" />
-          <div class="flex items-center gap-1 w-20">
-            <div class="w-full h-full flex items-start gap-[2px]">
-              <span
-                class="poppins-medium scroll-m-20 text-lg font-semibold tracking-tight text-red-600"
-                >25</span
-              >
-              <span class="text-xs font-normal mt-[3px]">&#8451;</span>
-            </div>
-            <div class="w-full h-full flex items-start gap-[2px]">
-              <span
-                class="poppins-medium scroll-m-20 text-lg font-semibold tracking-tight text-sky-600"
-                >10</span
-              >
-              <span class="text-xs font-normal mt-[3px]">&#8451;</span>
-            </div>
-          </div>
-        </div>
-        <div class="flex-1 flex items-center justify-end gap-5 mb-1">
-          <small class="text-sm leading-none">Nov 30</small>
-          <small class="text-sm leading-none">토요일</small>
-        </div>
-      </div>
-      <div
-        class="w-full flex items-center gap-7 bg-neutral-50 py-0 px-3 rounded-md"
-      >
-        <div class="w-fit h-10 flex items-center gap-2">
-          <img src="/assets/icons/1000d.svg" alt="" class="h-7 w-7" />
-          <div class="flex items-center gap-1 w-20">
-            <div class="w-full h-full flex items-start gap-[2px]">
-              <span
-                class="poppins-medium scroll-m-20 text-lg font-semibold tracking-tight text-red-600"
-                >25</span
-              >
-              <span class="text-xs font-normal mt-[3px]">&#8451;</span>
-            </div>
-            <div class="w-full h-full flex items-start gap-[2px]">
-              <span
-                class="poppins-medium scroll-m-20 text-lg font-semibold tracking-tight text-sky-600"
-                >10</span
-              >
-              <span class="text-xs font-normal mt-[3px]">&#8451;</span>
-            </div>
-          </div>
-        </div>
-        <div class="flex-1 flex items-center justify-end gap-5 mb-1">
-          <small class="text-sm leading-none">Nov 30</small>
-          <small class="text-sm leading-none">토요일</small>
-        </div>
-      </div>
-      <div
-        class="w-full flex items-center gap-7 bg-neutral-50 py-0 px-3 rounded-md"
-      >
-        <div class="w-fit h-10 flex items-center gap-2">
-          <img src="/assets/icons/1000d.svg" alt="" class="h-7 w-7" />
-          <div class="flex items-center gap-1 w-20">
-            <div class="w-full h-full flex items-start gap-[2px]">
-              <span
-                class="poppins-medium scroll-m-20 text-lg font-semibold tracking-tight text-red-600"
-                >25</span
-              >
-              <span class="text-xs font-normal mt-[3px]">&#8451;</span>
-            </div>
-            <div class="w-full h-full flex items-start gap-[2px]">
-              <span
-                class="poppins-medium scroll-m-20 text-lg font-semibold tracking-tight text-sky-600"
-                >10</span
-              >
-              <span class="text-xs font-normal mt-[3px]">&#8451;</span>
-            </div>
-          </div>
-        </div>
-        <div class="flex-1 flex items-center justify-end gap-5 mb-1">
-          <small class="text-sm leading-none">Nov 30</small>
-          <small class="text-sm leading-none">토요일</small>
-        </div>
-      </div>
-      <div
-        class="w-full flex items-center gap-7 bg-neutral-50 py-0 px-3 rounded-md"
-      >
-        <div class="w-fit h-10 flex items-center gap-2">
-          <img src="/assets/icons/1000d.svg" alt="" class="h-7 w-7" />
-          <div class="flex items-center gap-1 w-20">
-            <div class="w-full h-full flex items-start gap-[2px]">
-              <span
-                class="poppins-medium scroll-m-20 text-lg font-semibold tracking-tight text-red-600"
-                >25</span
-              >
-              <span class="text-xs font-normal mt-[3px]">&#8451;</span>
-            </div>
-            <div class="w-full h-full flex items-start gap-[2px]">
-              <span
-                class="poppins-medium scroll-m-20 text-lg font-semibold tracking-tight text-sky-600"
-                >10</span
-              >
-              <span class="text-xs font-normal mt-[3px]">&#8451;</span>
-            </div>
-          </div>
-        </div>
-        <div class="flex-1 flex items-center justify-end gap-5 mb-1">
-          <small class="text-sm leading-none">Nov 30</small>
-          <small class="text-sm leading-none">토요일</small>
-        </div>
-      </div>
-      <div
-        class="w-full flex items-center gap-7 bg-neutral-50 py-0 px-3 rounded-md"
-      >
-        <div class="w-fit h-10 flex items-center gap-2">
-          <img src="/assets/icons/1000d.svg" alt="" class="h-7 w-7" />
-          <div class="flex items-center gap-1 w-20">
-            <div class="w-full h-full flex items-start gap-[2px]">
-              <span
-                class="poppins-medium scroll-m-20 text-lg font-semibold tracking-tight text-red-600"
-                >25</span
-              >
-              <span class="text-xs font-normal mt-[3px]">&#8451;</span>
-            </div>
-            <div class="w-full h-full flex items-start gap-[2px]">
-              <span
-                class="poppins-medium scroll-m-20 text-lg font-semibold tracking-tight text-sky-600"
-                >10</span
-              >
-              <span class="text-xs font-normal mt-[3px]">&#8451;</span>
-            </div>
-          </div>
-        </div>
-        <div class="flex-1 flex items-center justify-end gap-5 mb-1">
-          <small class="text-sm leading-none">Nov 30</small>
-          <small class="text-sm leading-none">토요일</small>
-        </div>
-      </div>
-      <div
-        class="w-full flex items-center gap-7 bg-neutral-50 py-0 px-3 rounded-md"
-      >
-        <div class="w-fit h-10 flex items-center gap-2">
-          <img src="/assets/icons/1000d.svg" alt="" class="h-7 w-7" />
-          <div class="flex items-center gap-1 w-20">
-            <div class="w-full h-full flex items-start gap-[2px]">
-              <span
-                class="poppins-medium scroll-m-20 text-lg font-semibold tracking-tight text-red-600"
-                >25</span
-              >
-              <span class="text-xs font-normal mt-[3px]">&#8451;</span>
-            </div>
-            <div class="w-full h-full flex items-start gap-[2px]">
-              <span
-                class="poppins-medium scroll-m-20 text-lg font-semibold tracking-tight text-sky-600"
-                >10</span
-              >
-              <span class="text-xs font-normal mt-[3px]">&#8451;</span>
-            </div>
-          </div>
-        </div>
-        <div class="flex-1 flex items-center justify-end gap-5 mb-1">
-          <small class="text-sm leading-none">Nov 30</small>
-          <small class="text-sm leading-none">토요일</small>
+          <small class="text-sm leading-none">{{
+            formatDate(Number(item.date))
+          }}</small>
+          <small class="text-sm leading-none">{{
+            formatDay(Number(item.date))
+          }}</small>
         </div>
       </div>
     </CardContent>
@@ -199,11 +54,41 @@
 </template>
 
 <script setup lang="ts">
+import { toRefs } from "vue";
 import Card from "../ui/card/Card.vue";
 import CardContent from "../ui/card/CardContent.vue";
 import CardDescription from "../ui/card/CardDescription.vue";
 import CardHeader from "../ui/card/CardHeader.vue";
 import CardTitle from "../ui/card/CardTitle.vue";
+
+interface WeatherInfo {
+  maxTemp: number;
+  minTemp: number;
+  date: string;
+  iconCode: number;
+  isDay: boolean;
+}
+
+interface Props {
+  data: WeatherInfo[];
+}
+
+const props = defineProps<Props>();
+const { data } = toRefs(props);
+
+const formatDate = (epoch: number) => {
+  const date = new Date(epoch * 1000);
+  const day = date.toLocaleDateString("en-EN", { day: "2-digit" });
+  const month = date.toLocaleString("en-EN", { month: "short" });
+
+  return `${day} ${month}`;
+};
+const formatDay = (epoch: number) => {
+  const date = new Date(epoch * 1000);
+  const weekday = date.toLocaleDateString("ko-KR", { weekday: "long" });
+
+  return weekday;
+};
 </script>
 
 <style lang="scss" scoped></style>
--- vue-weather/src/views/HomeView.vue
+++ vue-weather/src/views/HomeView.vue
@@ -15,7 +15,7 @@
         <!-- 하단 위젯 영역 -->
         <div class="w-full flex items-center gap-6">
           <WidgetHighlightWeather :data="dataOfHLW" />
-          <WidgetOneWeekWeather />
+          <WidgetOneWeekWeather :data="dataOf7DW" />
         </div>
       </div>
     </div>
@@ -33,6 +33,18 @@ import {
 } from "@/components/widgets";
 import axios from "axios";
 import { onMounted, reactive, ref } from "vue";
+
+interface ForecastDay {
+  day: {
+    maxtemp_c: number;
+    mintemp_c: number;
+    condition: {
+      code: number;
+      icon: string;
+    };
+  };
+  date_epoch: string;
+}
 
 const cityName = ref<string>("seoul");
 const dataOfCW = reactive({
@@ -54,13 +66,13 @@ const dataOfHLW = reactive({
   visibility: 0,
   feelsLike: 0,
 });
+const dataOf7DW = ref([]);
 
 const fetchApi = async () => {
   try {
     const res = await axios.get(
       `${import.meta.env.VITE_WEATHER_BASE_URL}/forecast.json?q=${cityName.value}&days=7&key=${import.meta.env.VITE_WEATHER_API_KEY}`,
     );
-    console.log(res);
     const { current, location, forecast } = res.data;
 
     // 현재 날씨 위젯 컴포넌트에서 필요한 데이터
@@ -83,6 +95,17 @@ const fetchApi = async () => {
     dataOfHLW.pressure = current.pressure_mb;
     dataOfHLW.visibility = current.vis_km;
     dataOfHLW.feelsLike = current.feelslike_c;
+
+    // 주간 날씨 위젯 컴포넌트에서 필요한 데이터
+    dataOf7DW.value = forecast.forecastday.map((item: ForecastDay) => {
+      return {
+        maxTemp: Math.round(item.day.maxtemp_c),
+        minTemp: Math.round(item.day.mintemp_c),
+        date: item.date_epoch,
+        iconCode: item.day.condition.code,
+        isDay: item.day.condition.icon.includes("day"),
+      };
+    });
   } catch (err) {
     console.log(err);
   }
