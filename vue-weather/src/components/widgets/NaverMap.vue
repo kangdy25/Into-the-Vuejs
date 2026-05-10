0be106c155b2c576d77c15d0396715750c31389d
Kangdy <canfrog@naver.com>
2026-05-10 19:43:31 +0900

    [Vue Project - Weather] Implement real-time weather data fetching

--- vue-weather/src/components/widgets/NaverMap.vue
+++ vue-weather/src/components/widgets/NaverMap.vue
@@ -1,24 +1,42 @@
 <template>
   <Card class="w-1/4 min-w-[25%] h-[260px] p-0 overflow-hidden">
-    <NaverMap
-      style="width: 100%; height: 100%"
-      :map-options="mapOptions"
-      @on-load="onLoadMap"
-    />
+    <NaverMap style="width: 100%; height: 100%" :map-options="mapOptions">
+      <NaverMarker
+        v-for="position in positions"
+        :latitude="position.lat"
+        :longitude="position.lng"
+        @onLoad="onLoadMarker($event)"
+        @click="handleSearch(position.cityName)"
+      />
+    </NaverMap>
   </Card>
 </template>
 
 <script setup lang="ts">
-import { NaverMap } from "vue3-naver-maps";
+import { NaverMap, NaverMarker } from "vue3-naver-maps";
 import { Card } from "@/components/ui/card";
+import { useStore } from "@/store";
+import { ref } from "vue";
+import positions from "@/constant/map-position.json";
+
+const store = useStore();
+
+const marker = ref();
 
 const mapOptions = {
-  latitude: 37.51347,
+  latitude: 36.78347,
   longitude: 127.041722,
-  zoom: 14,
+  zoom: 7,
+  zoomControl: false,
+  zoomControlOptions: { position: "TOP_RIGHT" },
 };
 
-const onLoadMap = (map: any) => {
-  console.log("Map loaded:", map);
+const onLoadMarker = (markerObject: any) => {
+  marker.value = markerObject;
+};
+
+const handleSearch = (selected: string) => {
+  store.cityName = selected;
+  store.fetchApi();
 };
 </script>
