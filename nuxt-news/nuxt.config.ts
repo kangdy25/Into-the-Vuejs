b79f69484c8f1481d11eb10ec4d86ac5439fa454
Shineast <canfrog@naver.com>
2026-05-11 20:45:05 +0900

    [Nuxt Project - News] Development Environment Setting

---
+++ nuxt-news/nuxt.config.ts
@@ -0,0 +1,16 @@
+import tailwindcss from "@tailwindcss/vite";
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  css: ["./app/style/index.css"],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  shadcn: {
+    prefix: "",
+    componentDir: "./components/ui",
+  },
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+  modules: ["shadcn-nuxt"],
+});
