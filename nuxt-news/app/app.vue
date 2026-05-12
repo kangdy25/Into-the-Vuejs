1923a827a169e06e52ca856a190d750ae2671185
Shineast <canfrog@naver.com>
2026-05-12 11:09:51 +0900

    [Nuxt Project - News] Prepare UI components

--- nuxt-news/app/app.vue
+++ nuxt-news/app/app.vue
@@ -1,6 +1,42 @@
+<script setup lang="ts">
+import AppHeader from "./components/common/AppHeader.vue";
+</script>
+
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="app">
+    <AppHeader />
+    <NuxtPage />
   </div>
 </template>
+
+<style>
+body {
+  overflow-x: hidden;
+}
+
+.app {
+  width: 100%;
+  height: 100%;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+}
+
+.page {
+  width: 100%;
+  max-width: 1132px;
+  height: 100%;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+
+  margin-top: 57px;
+  padding: 24px 0;
+
+  gap: 24px;
+}
+</style>
---
+++ nuxt-news/app/components/common/AppHeader.vue
@@ -0,0 +1,34 @@
+<template>
+  <header class="header">
+    <div class="flex items-center">
+      <img src="/assets/icons/icon-green.svg" alt="" class="h-10" />
+      <h3 class="scroll-m-20 text-2xl font-semibold tracking-tight">
+        Nuxt4 News
+      </h3>
+    </div>
+    <div class="flex items-center">
+      <Avatar class="cursor-pointer">
+        <AvatarImage src="https://github.com/9diin.png" alt="profile-image" />
+        <AvatarFallback>CN</AvatarFallback>
+      </Avatar>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+.header {
+  position: fixed;
+  z-index: 1;
+
+  width: 100%;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  padding: 8px 24px;
+
+  background-color: #ffffff;
+  border-bottom: 1px solid #f5f5f5;
+}
+</style>
